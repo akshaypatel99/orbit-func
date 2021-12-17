656770f60f68cba8351719448e6e791890f6f5bc
akshaypatel99 <akshaypatel99@gmail.com>
2021-12-17 19:20:47 +0000

    Add first function

--- functions/ping.js
+++ functions/ping.js
@@ -1,6 +1,6 @@
 exports.handler = function (event, context, callback) {
 	return callback(null, {
 		statusCode: 200,
-		body: JSON.stringify({ 'Hello from a serverless function!': 'world' }),
+		body: 'Hello from the serverless Orbit API!',
 	});
 };
